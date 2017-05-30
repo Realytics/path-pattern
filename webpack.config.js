92234a5ef1cc82bd4a5cc82908c2972e52ad7975
Etienne Dldc <etienne.dldc@outlook.fr>
2017-05-30 11:12:15 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+var path = require('path');
+var webpack = require('webpack');
+var process = require('process');
+
+var env = process.env.NODE_ENV;
+
+var config = {
+  context: __dirname,
+  target: 'web',
+  name: 'redux-router-location',
+  entry: './src/index.ts',
+
+  output: {
+    library: 'ReduxRouterLocation',
+    libraryTarget: 'umd'
+  },
+
+  devtool: 'source-map',
+
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx']
+  },
+
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin()
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        include: path.join(__dirname, 'src'),
+        options: {
+          configFileName: './tsconfig.json',
+          silent: false,
+          visualStudioErrorFormat: true
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'tslint-loader',
+        include: path.join(__dirname, 'src'),
+        enforce: 'pre'
+      }
+    ]
+  },
+
+  externals: {
+    'path-to-regexp': {
+      root: 'PathToRegexp',
+      commonjs2: 'path-to-regexp',
+      commonjs: 'path-to-regexp',
+      amd: 'path-to-regexp'
+    }
+  }
+};
+
+if ('production' === env) {
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compressor: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false,
+        screw_ie8: false
+      },
+      mangle: {
+        screw_ie8: false
+      },
+      output: {
+        screw_ie8: false
+      }
+    })
+  );
+}
+
+module.exports = config;
